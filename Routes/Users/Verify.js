f658c2dbe4404d7a7b9f9281faae718e1696a9ed
Dhairya Singla <dhairyasingla250504@gmail.com>
2024-04-07 18:05:05 +0530

    Verified user signup only

---
+++ Routes/Users/Verify.js
@@ -0,0 +1,56 @@
+import { Router } from 'express';
+import { UserDuplicacy } from '../../Middlewares/Users/UserDuplicacy.js';
+import { pool } from '../../Database/Database.js';
+import { nanoid } from 'nanoid';
+
+export const VerifyRoute = Router();
+
+VerifyRoute.get('/', UserDuplicacy, async (req,res) => {
+    
+    const email = req.query.m;
+
+    let client;
+
+    try {
+        client = await pool.connect();
+        try {
+            const id = nanoid(20);
+            client.query(`SELECT * FROM verificationtable WHERE EMAIL = $1;`,[email],(err,result) => {
+                if(err){
+                    res.status(500).json({
+                        'Error' : 'Query not working'
+                    })
+                }else{
+                    client.query(`INSERT INTO users (ID, NAME, EMAIL, PASSWORD, PICTURE) VALUES ($1,$2,$3,$4,$5);`,[id,result.rows[0].name,result.rows[0].email,result.rows[0].password,""],(err,result) => {
+                        if(err){
+                            res.status(500).json({
+                                'Error' : 'Query not working'
+                            })
+                        }else{
+                            client.query(`DELETE FROM verificationtable WHERE EMAIL = $1;`,[email],(err,result) => {
+                                if(err){
+                                    res.status(500).json({
+                                        'Error' : 'Query not working'
+                                    })
+                                }
+                                else{
+                                    res.json({
+                                        msg : "User created successfully",
+                                    })
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+        } catch (error) {
+            res.status(500).json({
+                'Error' : 'Internal server Error',
+            })
+        }
+    } catch (error) {
+        res.status(500).json({
+            'Error' : "Could not connect to database",
+        })
+    }
+})
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { SignUpRoute } from './Routes/Users/SignUp.js';
+import { VerifyRoute } from './Routes/Users/Verify.js';
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use('/signup',SignUpRoute);
+app.use('/verify',VerifyRoute);
 
 const PORT = process.env.PROT || 7001;
 app.listen(PORT,() => {
